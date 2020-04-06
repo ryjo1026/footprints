61491ec2f7714103119ddd4b7e82f6f170201406
Ryan Johnston <ryjo1026@gmail.com>
2020-04-06 15:45:33 -0400

    Initial commit

---
+++ src/js/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from "./components/Home.jsx";
+
+ReactDOM.render(<Home />, document.getElementById("container"));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/App.jsx',
+  output: {
+    path: path.join(__dirname, '/build/static/js/'),
+    publicPath: '/static/js/',
+    filename: './build/static/js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    watchOptions: {
+      poll: true,
+    },
+    contentBase: path.join(__dirname, 'build'),
+    publicPath: '/static/js/',
+    compress: true,
+    port: 9000,
+  },
+};

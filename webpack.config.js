b31e99eaae79fa04a87b61ce6163c8397d7d7bc0
Ryan Johnston <ryjo1026@gmail.com>
2020-04-07 16:29:14 -0400

    Added bulma and sass loading

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,14 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     watchOptions: {

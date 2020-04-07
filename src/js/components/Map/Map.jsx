6032701708da0702d8a8adaa5c2d87d344c17294
anders617 <anders617@gmail.com>
2020-04-07 16:43:07 -0400

    Snap points to road

--- src/js/components/Map/Map.jsx
+++ src/js/components/Map/Map.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, KmlLayer } from 'react-google-maps';
 import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
+import { getNearestRoads } from '../../api/map.js';
 
 const createMockData = () => {
     let data = [];
-    for (let i = 0; i < 1000; i++) {
-        data.push(new google.maps.LatLng({ lat: Math.random() * 0.02 + 42.27, lng: Math.random() * 0.02 - 83.753 }));
+    for (let i = 0; i < 50; i++) {
+        data.push(new google.maps.LatLng({ lat: Math.random() * 0.01 + 42.275, lng: Math.random() * 0.01 - 83.752 }));
     }
     return data;
 };
 
 export default withScriptjs(withGoogleMap((props) => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getNearestRoads({points: createMockData()})
+            .then((res) => res.map((d) => new google.maps.LatLng({lat: d.latitude, lng: d.longitude})))
+            .then((res) => setData(res));
+    }, []);
     return (
         <GoogleMap
             defaultZoom={14}
@@ -18,7 +25,7 @@ export default withScriptjs(withGoogleMap((props) => {
         >
             {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
             <HeatmapLayer
-                data={createMockData()}
+                data={data}
                 options={{
                     gradient: ['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 1)', 'rgb(0, 0, 200)', 'rgb(0, 0, 255)']
                 }}

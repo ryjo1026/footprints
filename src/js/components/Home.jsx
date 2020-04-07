43e491f7c25796566571ae6ade95583c9816e55a
anders617 <anders617@gmail.com>
2020-04-07 14:17:37 -0400

    Testing map layers

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from './Map/index.jsx';
 
+const MAPS_URL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCOzeOHT1tovidiIL9uVNODS0REn9Ciy-s';
+
 export default function Home() {
   return <Map 
     isMarkerShown
-    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+    googleMapURL={MAPS_URL}
     loadingElement={<div style={{ height: `100%` }} />}
     containerElement={<div style={{ height: `100%` }} />}
     mapElement={<div style={{ height: `100%` }} />}

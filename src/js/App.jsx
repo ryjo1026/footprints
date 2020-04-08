99814d99d472199998324051165c1d6353686ab5
anders617 <anders617@gmail.com>
2020-04-07 20:11:36 -0400

    Added maps style

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import Home from './components/Home.jsx';
 
 ReactDOM.render(<Home />, document.getElementById('container'));
